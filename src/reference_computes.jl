3aac4c74651abd24d505cc9998d23513c0b508af
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-04-18 16:27:49 +0200

    Couple more variables and conversion funcs

--- src/reference_computes.jl
+++ src/reference_computes.jl
@@ -8,6 +8,8 @@ function p_T_rho_L(p_ref, T_ref, rho_ref, L_ref)
     n_ref = p_ref / (k_B * T_ref)
     m_ref = rho_ref / n_ref
     v_ref = v_ref(p_ref, rho_ref)
+    t_ref = L_ref / v_ref
+    μ_ref = p_ref * t_ref
 
     return Dict("p_ref" => p_ref,
                 "T_ref" => T_ref,
@@ -18,12 +20,18 @@ function p_T_rho_L(p_ref, T_ref, rho_ref, L_ref)
                 "e_ref" => k_B * T_ref / m_ref,
                 "cv_ref" => k_B / m_ref,
                 "R_specific_ref" => k_B / m_ref,
-                "t_ref" => L_ref / v_ref,
-                "μ_ref" => p_ref * t_ref,
+                "t_ref" => t_ref,
+                "μ_ref" => μ_ref,
+                "Reynolds_ref" => rho_ref * v_ref * L_ref / μ_ref
                 )
 end
 
 function p_m_rho_L(p_ref, m_ref, rho_ref, L_ref)
     T_ref = m_ref / k_B * p_ref / rho_ref
+    return p_T_rho_L(p_ref, T_ref, rho_ref, L_ref)
+end
+
+function T_v_rho_L(T_ref, v_ref, rho_ref, L_ref)
+    p_ref = rho_ref * v_ref^2
     return p_T_rho_L(p_ref, T_ref, rho_ref, L_ref)
 end
