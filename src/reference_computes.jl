d2637b0cb66d2932cf42033210eb018002eb6eb9
Georgii Oblapenko <kunstmord@kunstmord.com>
2024-05-31 14:27:41 +0200

    Update reference_computes.jl

--- src/reference_computes.jl
+++ src/reference_computes.jl
@@ -1,13 +1,13 @@
 const k_B = 1.380649e-23
 
-function v_ref(p_ref, rho_ref)
+function get_v_ref(p_ref, rho_ref)
     return sqrt(p_ref/rho_ref)
 end
 
 function p_T_rho_L(p_ref, T_ref, rho_ref, L_ref)
     n_ref = p_ref / (k_B * T_ref)
     m_ref = rho_ref / n_ref
-    v_ref = v_ref(p_ref, rho_ref)
+    v_ref = get_v_ref(p_ref, rho_ref)
     t_ref = L_ref / v_ref
     μ_ref = p_ref * t_ref
 
